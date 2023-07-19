60a062bdd2304410ddea091ce04bb8d7387f1a48
Luis Miguel Casado Diaz <luicasad@cbr4s3.42barcelona.com>
2023-07-19 13:46:32 +0200

    C02 EX11

--- c_02/ex11/ft_putstr_non_printable.c
+++ c_02/ex11/ft_putstr_non_printable.c
@@ -6,9 +6,63 @@
 /*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 18:42:43 by luicasad          #+#    #+#             */
-/*   Updated: 2023/07/13 18:43:07 by luicasad         ###   ########.fr       */
+/*   Updated: 2023/07/19 13:37:55 by luicasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_putstr_non_printable(char *str);
+#include <unistd.h>
 
+/* ************************************************************************** */
+/* ft_hex_of prints a integer < 128 .                                         */
+/* NON printable char are printed according to the theme                      */
+/* 	theme = 0 print a point '.'                                               */
+/*  theme = 1 print wiht slash  '\0a'                                         */	
+/*  theme = 2 print regular template '0a'                                     */
+/* ************************************************************************** */
+void	ft_hex_of(int num, int theme)
+{
+	int	dot;
+	int	backslash;
+
+	dot = 46;
+	backslash = 92;
+	if (theme == 0)
+		write(1, &dot, 1);
+	else
+	{
+		if (theme == 1)
+			write(1, &backslash, 1);
+		write(1, &"0123456789abcdef"[num / 16], 1);
+		write(1, &"0123456789abcdef"[num % 16], 1);
+	}
+}
+
+int	ft_in_rng(int min, int max, int c)
+{
+	if (min <= c && c <= max)
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_print_char(int c)
+{
+	return (ft_in_rng(32, 126, c));
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int	idx;
+
+	if (str != NULL)
+	{
+		idx = -1;
+		while (str[++idx] != '\0')
+		{
+			if (ft_print_char((int)str[idx]))
+				write(1, &str[idx], 1);
+			else
+				ft_hex_of((int)str[idx], 1);
+		}
+	}
+}
--- c_02/ex11/test.c
+++ c_02/ex11/test.c
@@ -6,50 +6,32 @@
 /*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 20:18:47 by luicasad          #+#    #+#             */
-/*   Updated: 2023/07/13 18:43:25 by luicasad         ###   ########.fr       */
+/*   Updated: 2023/07/19 13:45:59 by luicasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/* ************************************************************************** */
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
-#include <stdio.h>
+void	ft_putstr(char *str)
+{
+	int	idx;
 
- void ft_putstr_non_printable(char *str);
+	if (str != NULL)
+	{
+		idx = 0;
+		while (str[idx] != '\0')
+			write(1, &str[idx++], 1);
+	}
+}
+
+char	*ft_putstr_non_printable(char *str);
 
 int	main(void)
 {
-	int	tabla[12];
-	int	*ptabla;
-	int	idx;
+	char	t5[] = "  salut, c\aomm\bent tu vas ? 42\tmots quarante\r-deux; cinquante+et+un";
 
-	tabla[0] = 66;
-	tabla[1] = 55;
-	tabla[2] = 44;
-	tabla[3] = 33;
-	tabla[4] = 22;
-	tabla[5] = 1212;
-	tabla[6] = 88;
-	tabla[7] = 99;
-	tabla[8] = 1010;
-	tabla[9] = 11;
-	tabla[10] = 1111;
-	tabla[11] = 77;
-	ptabla = &tabla[0];
-	idx = 0;
-	while (idx < 12)
-		printf("%d\t", tabla[idx++]);
-	printf("\n");
-	idx = *(&tabla + 1) - tabla;
-	ft_sort_int_tab(ptabla, 7);
-	idx = 0;
-	while (idx < 12)
-		printf("%d\t", tabla[idx++]);
-	printf("\n");
-	ft_sort_int_tab(ptabla, idx);
-	idx = 0;
-	while (idx < 12)
-		printf("%d\t", tabla[idx++]);
-	printf("\n");
-	ft_sort_int_tab(ptabla, 0);
-	ft_sort_int_tab(NULL, idx);
-	ft_sort_int_tab(NULL, 0);
+	ft_putstr(t5);
+	ft_putstr("\n");
+	ft_putstr_non_printable(t5);
 }
